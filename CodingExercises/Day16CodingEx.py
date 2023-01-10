05adc00709647629e5081dfdf868da20bb1d0c81
jacksteel <jacksteel531@icloud.com>
2023-01-10 14:33:45 -0500

    Edit

--- CodingExercises/Day16CodingEx.py
+++ CodingExercises/Day16CodingEx.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 
+sg.theme("Black")
+
 label_one = sg.Text("Enter feet: ")
 input_one = sg.Input(key="feet")
 
@@ -9,10 +11,14 @@ input_two = sg.Input(key="inches")
 output_label = sg.Text("", key="output")
 
 convert_button = sg.Button("Convert")
-convert = sg.Window("Converter", [[label_one, input_one],[label_two, input_two],[convert_button, output_label]])
+exit_button = sg.Button("Exit")
+convert = sg.Window("Converter", [[label_one, input_one],[label_two, input_two],[convert_button, exit_button, output_label]])
 
 while True:
     event, values = convert.read()
+    match event:
+        case "Exit":
+            break
     feet = float(values['feet'])
     inches = float(values['inches'])
     meter = feet * 0.3048 + inches * .0254
